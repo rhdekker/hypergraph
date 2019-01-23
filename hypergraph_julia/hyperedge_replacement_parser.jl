5defa5f564bf53f734b5f71f974bf0cf57168dc4
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-01-23 10:19:44 +0100

    Replaced for loops with list comprehensions.

--- hypergraph_julia/hyperedge_replacement_parser.jl
+++ hypergraph_julia/hyperedge_replacement_parser.jl
@@ -78,21 +78,14 @@ function he_replace(state_machine::StateMachine, label::String)
 
     iter_source_external_nodes = Iterators.Stateful(hyperedge_to_replace.source)
     iter_target_external_nodes = Iterators.Stateful(hyperedge_to_replace.target)
+
     copy_hyperedges::Array{HyperEdge} = []
     for edge in hypergraph_to_replace_with
         # ga over alle source node en kopieer als nodig
-        copy_source_nodes::Array{Node} = []
-        for source_node in edge.source
-            a = source_node == "_" ? popfirst!(iter_source_external_nodes) : source_node
-            push!(copy_source_nodes, a)
-        end
-#         println(copy_source_nodes)
+        copy_source_nodes = [source_node == "_" ? popfirst!(iter_source_external_nodes) : source_node for source_node in edge.source]
         # ga over alle target node en kopieer als nodig
-        copy_target_nodes::Array{Node} = []
-        for target_node in edge.target
-            a = target_node == "_" ? popfirst!(iter_target_external_nodes) : target_node
-            push!(copy_target_nodes, a)
-        end
+        copy_target_nodes = [target_node == "_" ? popfirst!(iter_target_external_nodes) : target_node for target_node in edge.target]
+#         println(copy_source_nodes)
 #         println(copy_target_nodes)
         push!(copy_hyperedges, HyperEdge(edge.label, copy_source_nodes, copy_target_nodes))
     end
